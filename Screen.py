054250be4444a80d4616e34639721ecf99d566e6
talcalf-ui <talcalf@post.bgu.ac.il>
2026-04-26 16:47:16 +0300

    Update Screen.py
    בתקווה שהפעם הלחיצה על רווח תעבוד

--- Screen.py
+++ Screen.py
@@ -44,14 +44,16 @@ class Screen(tk.Tk):
 class EyesPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-
-        master.bind_all("<space>", lambda event: self.reboot)
-        master.focus_force()
-        
         image = Image.open('pictures//eyes.png')
         self.photo_image = ImageTk.PhotoImage(image)  # self. - for keeping the photo in memory so it will be shown
         tk.Label(self, image=self.photo_image).pack()
 
+        master.bind_all("<space>", self.on_space)
+        master.focus_force()
+
+    def on_space(self, event):
+        print("SPACE DETECTED")  # לבדיקה
+        self.after(0, self.reboot)
 
     def reboot(self):
         print("Reboot performed: Resetting inter_aff and hardwere_aff")
